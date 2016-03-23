1ffed1d8f808d925c10a64e3d4f28f01c3b2582f
mohamey <mohamey@tcd.ie>
2016-03-23 23:00:12 +0000

    Lesson 1 Completed
    Hard coded some data in the main fragment

--- app/src/main/java/mohamey/com/sunshine/MainActivityFragment.java
+++ app/src/main/java/mohamey/com/sunshine/MainActivityFragment.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> weather = new ArrayList<String>();
+        weather.add("Today - Sunny - 88/63");
+        weather.add("Tomorrow - Foggy - 66/69");
+        weather.add("Weds - Cloudy - 72/63");
+        weather.add("Thurs - Rainy - 64/51");
+        weather.add("Fri - Foggy - 70/46");
+        weather.add("Sat - Sunny - 76/68");
+
+        ArrayAdapter<String> weatherAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weather);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(weatherAdapter);
+        return rootView;
     }
 }
